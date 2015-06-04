a3b86e73cb60507540246628baaec04eb489dc85
ohanhi <ossi.hanhinen@gmail.com>
2015-06-04 14:58:57 +0300

    Add source files

---
+++ SmallDataSource.elm
@@ -0,0 +1,41 @@
+module SmallDataSource (alastalo) where
+
+-- Retrieved from http://wunderdog.fi/static/alastalon_salissa.zip
+alastalo = """
+Suvinen, sunnuntainen ehtoopuoli. Hiljainen saaristokirkko hautausmaan
+keskellä. Kirkon musta paanukatto kohoo korkeana ja vakavana
+lehviköissään, läntisen auringon välke leikkii sen punaisella
+seinämällä kirkastaen valkeat akkunanpielet. Sanomaton rauha ilmassa ja
+ylt'ympärillä. Ainoana äänenä vain haapojen lepatus sammaltuneen
+kiviaidan vierillä.
+
+Olen aukaissut kirkkotarhan rautaportin, vingahtaen kumahtaa se kiinni
+takanani saranoillaan ja käytävän somer narahtaa jalkojeni alla.
+Harmaita haaltuneita puuristejä kahden puolen, tuolla täällä musta
+malmiristi messinkikilvin, seassa muita korkeampana joku
+rautatangolleen pujotettu peltilevy, jonka kaartuvalta pinnalta
+himertää silmään ruostunut, sammaltunut kirjoitus. Hiljaisempaako vielä
+ristimetsässä, kuin äsken pyhäehtoiseen lepoonsa suljetun rautaportin
+suulla?
+
+Pujottelen ristien lomitse, tapailen silmilläni haaltuneita
+kirjoituksia, selvitän nimen siellä, nimen täällä. Sinäkö siinä lepäät
+ja Sinäkö tämän turpeen turvissa? Nimeä nimen jälkeen, tuttua kuin oma
+lapsuus, tavailen; yksitellen, kumpu kummulta, rivi riviltä heitä luen:
+Hänkin siis ja myöskin Hän, nuori, vanha, nuoruuteni kansa, herkkä
+lapsi, tutiseva vanhus, vieri vierin somereen suojissa, kuinka on
+vuorollensa kumahdellut kuolinkello! Vilahtaa mieleeni ammoisten
+muistojen komeroista aivinaisella pieluksella lepäävä pikkutyttösen
+pää, riutuneilla ohimoilla sinervät suonet, kalpealla laihtuneella
+poskella joku eksynyt köyhä tuhanvärinen hiussuortuva, harmaissa
+silmissä orpo, elämän toivon heittänyt kaipauksen kipeä kuulto.
+Hämärtää silmääni kumara vanhus, tutiseva, kyhmyinen käsi kuluneella
+sauvan koukulla, ohuet hallavan valkeat hiussuortuvain rippeet kuin
+kurttuihinsa haihtuvien, kärsimyksellisesti kalpeiden kasvojen
+ympärillä, jotka elämä jo on riisunut niin tyyten, ettei kuolemalla
+liene ollut suurta työtä niiden kasvojen sammuttamisessa. Kummulta
+kummulle, ristiltä ristille vaellan, luen sammaltuneita kirjoituksia
+sammuneitten ihmisten haudoilla, sammuneitten, jo vuosia ja
+vuosikymmeniä sitten sammuneitten, joitten elämä kuitenkin on edessäni
+kuin eilinen päivä.
+"""
---
+++ WunderPahkina.elm
@@ -0,0 +1,96 @@
+import Graphics.Element exposing (..)
+import String
+import Array
+import List
+import Set
+
+-- DataSource containts the book "Alastalon Salissa" by Volter Kilpi
+import DataSource
+
+type alias WordPair =
+  { wordA : String, wordB : String, muhkeus : Int }
+
+-- Valid characters from the exercise definition
+validChars : Set.Set Char
+validChars =
+  Set.fromList
+    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'
+    , 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'
+    , 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö'
+    ]
+
+-- how many best single words to use as the base for cross-examination
+initialTake = 20
+
+main : Element
+main =
+  DataSource.alastalo
+    |> bestWordPairs
+    |> show
+
+{-
+Get `WordPair`s with most "muhkeus" in `text`.
+In essence we
+  1. find N best single words as the base sample
+  2. cross-examine the sample to get best pairs
+  3. find highest "muhkeus" within the pairs
+  4. take all pairs with the highest possible "muhkeus"
+-}
+bestWordPairs : String -> List WordPair
+bestWordPairs text =
+  let bestSingleWords =
+        text
+          |> String.words
+          |> bestWords initialTake
+      bestPairs =
+        bestSingleWords
+          |> List.map (bestMatches bestSingleWords)
+          |> List.concat
+      highestMuhkeus =
+        bestPairs
+          |> List.map (\wp -> wp.muhkeus)
+          |> List.maximum
+          |> Maybe.withDefault 0
+  in  bestPairs
+        |> List.filter (\wp -> wp.muhkeus == highestMuhkeus)
+
+-- check best matches for `wordA` from `words` list
+bestMatches : List String -> String -> List WordPair
+bestMatches words wordA =
+  let wl    = String.length wordA
+      wordPairs =
+        words
+          |> List.map (\w -> { wordA   = wordA
+                             , wordB   = w
+                             , muhkeus = tupleMuhkeus(wordA,w)
+                             })
+          |> List.sortBy .muhkeus
+          |> List.reverse
+          |> List.take 5
+  in  wordPairs
+
+-- get n words with most "muhkeus" from a list
+bestWords : Int -> List String -> List String
+bestWords n words =
+  words
+    |> List.sortBy muhkeus
+    |> List.reverse
+    |> List.take n
+
+-- "muhkeus" of a word tuple
+tupleMuhkeus : (String, String) -> Int
+tupleMuhkeus (a, b) =
+  String.append a b
+    |> muhkeus
+
+-- the "muhkeus" of the string
+muhkeus : String -> Int
+muhkeus string =
+  string
+    |> String.toLower
+    |> String.toList
+    |> Set.fromList             -- Set contains only unique chars
+    |> Set.intersect validChars -- Intersect "filters" only valid chars
+    |> Set.toList               -- Set doesn't have a length, but List does
+    |> List.length
+
